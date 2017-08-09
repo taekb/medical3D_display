bdd50442dfe321737699b44411a1a03af1c1abfc
Daniel Jeong <taekb@users.noreply.github.com>
2017-08-09 14:22:12 +0900

    Update medical3D_display.py

--- medical3D_display.py
+++ medical3D_display.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
-# @author: Daniel Jeong
+#   Copyright 2017 Daniel Jeong (@taekb)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 import sys
 import numpy as np
@@ -7,9 +19,10 @@ import h5py
 
 from vispy import app, scene
 
-# Create a with a 3D viewport
+# Create a SceneCanvas object with a 3D viewport
 #fullscreen=True for fullscreen view of the canvas
 canvas = scene.SceneCanvas(title='LVall_10: GT(Left), Result(Right)', keys='interactive')
+#Add grid
 grid = canvas.central_widget.add_grid()
 
 print("Working..")
@@ -32,19 +45,17 @@ surface_GT = scene.visuals.Isosurface(array_GT, level=0.9,
                                    parent=vb_GT.scene)
 surface_GT.transform = scene.transforms.STTransform(translate=(-350, -170, -115))
 
-# Create isosurface visual for Result
+# Create isosurface visual for RT (Result)
 surface_RT = scene.visuals.Isosurface(array_RT, level=0.9,
 				   color=(0.9, 0.6, 0.3, 1), shading='smooth',
 				   parent=vb_RT.scene)
 surface_RT.transform = scene.transforms.STTransform(translate=(-350, -170, -115))
 
-# Add a 3D axis to keep us oriented
+# Add x,y,z axes
 axis_GT = scene.visuals.XYZAxis(parent=vb_GT.scene)
 axis_RT = scene.visuals.XYZAxis(parent=vb_RT.scene)
  
-# Use a 3D camera
-# Manual bounds; Mesh visual does not provide bounds yet
-# Note how you can set bounds before assigning the camera to the viewbox
+# Use a 3D Turntable camera
 cam = scene.TurntableCamera(elevation=30, azimuth=30)
 cam.set_range((-200, 200), (-200, 200), (-200, 200))
 vb_GT.camera = cam
